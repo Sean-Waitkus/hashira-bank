410cc542c4297ee02a9edd02f8c1093e20c82dc9
slorf <30660396+Sean-Waitkus@users.noreply.github.com>
2026-08-02 08:09:36 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
 
 const CONFIG = {
   // Google Apps Script web app URL (after deploying)
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby07vuPYqIX04dwXJWYjz6ezbP7apBOx_NSyMkUjph-4WE7fEyo2vWvEVG_zv6vImk4ag/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxUBTtYYxWnqp1J_ejfkWT6cqkf2yV34yMjw5RMF7NJYFUs031JaA5pMjygu7t1mRfN5A/exec',
 
   // Discord OAuth — Client ID only (secret stays on backend)
   DISCORD_CLIENT_ID: '1533142397758734356',
